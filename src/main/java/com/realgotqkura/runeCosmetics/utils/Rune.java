875075a023b692940dc0d01374f0722edfd6c764
Realgotqkura <vladianastasov06@gmail.com>
2026-03-25 10:30:48 +0200

    Explosion rune

--- src/main/java/com/realgotqkura/runeCosmetics/utils/Rune.java
+++ src/main/java/com/realgotqkura/runeCosmetics/utils/Rune.java
@@ -6,7 +6,7 @@ import org.bukkit.Particle;
 
 public enum Rune {
 
-    REDSTONE, EMERALD, FIRE, SOUL_FIRE, WRATH;
+    REDSTONE, EMERALD, FIRE, SOUL_FIRE, WRATH, EXPLOSION;
 
 
     public String getName(){
@@ -24,6 +24,7 @@ public enum Rune {
             case FIRE -> {return "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTNlOTdmYWI0NzUzYjc1YmE1YjBjMDM4YmVkMzc3YjE2MmJhMjhiN2E1ZTI5MGFiZmQwMThhNTU4MWFjNTM4OCJ9fX0=";}
             case SOUL_FIRE -> {return "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDIwY2I3M2YyMDdiMDdkMjU5Y2ExZWVmN2NhZGUzMDJjN2VhMTA0YjhhMGMyNDBiODk3YTU3Nzk3MTEyMmU1NiJ9fX0=";}
             case WRATH -> {return "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNGZiOTdiMTdjNjM5NTM5MjY1OGYzMjcxOGFhNDZiZWZhMWMzMWQzNTcyNjUxYzMwZjdkMmJmM2I5M2Y2ZWFkOSJ9fX0=";}
+            case EXPLOSION -> {return "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjgzMWQ1NzIxOTUwZWZhOWFhNzk5NjdlYjE5MTZhYmViNjRjOTc4ZWE4NTkzYTBjNjgzMzU0ODA5YzZjMzYxZCJ9fX0=";}
 
             default -> { return "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZmJiMzBmZDEyMTRkN2ViMGJmZWMyNTZhNDA2Mzk1ZDEzYmI4ZjMyYjQyYzkzMmE1ODJiNjBlNDIxZjE1NDIxIn19fQ=="; }
         }
@@ -36,6 +37,7 @@ public enum Rune {
             case FIRE ->{return ChatColor.of("#F7994D");}
             case SOUL_FIRE ->  {return ChatColor.of("#23C5CC");}
             case WRATH -> {return ChatColor.RED;}
+            case EXPLOSION -> {return ChatColor.of("#AB8684");}
 
 
             default -> {return ChatColor.BLACK;}
@@ -49,6 +51,7 @@ public enum Rune {
             case FIRE -> {return "♨";}
             case SOUL_FIRE -> {return "☉";}
             case WRATH -> {return "☠";}
+            case EXPLOSION -> {return "⚠";}
         }
 
         return "NO SYMBOL";
@@ -69,6 +72,7 @@ public enum Rune {
             case FIRE -> {return Particle.FLAME;}
             case SOUL_FIRE -> {return Particle.SOUL_FIRE_FLAME;}
             case WRATH -> {return Particle.ANGRY_VILLAGER;}
+            case EXPLOSION -> {return Particle.EXPLOSION;}
         }
 
         return null;
